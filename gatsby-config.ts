5837c5fe7ebc222f984b1c7865e3080ebe04fad5
xiayudi <xiayudi@meituan.com>
2022-06-15 20:41:41 +0800

    接入MDX

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,20 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: []
+  plugins: [
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `posts`,
+      path: `${__dirname}/src/mdx/`,
+    },
+  },
+  {
+    resolve: `gatsby-plugin-mdx`,
+    options: {
+      extensions: [`.mdx`, `.md`]
+    },
+  },]
 }
 
 export default config
--- src/pages/content.tsx
+++ src/pages/content.tsx
@@ -1,16 +1,11 @@
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Header from './Header';
-import MainFeaturedPost from './MainFeaturedPost';
-import FeaturedPost from './FeaturedPost';
-import Sidebar from './Sidebar';
 import Footer from './Footer';
+import { MDXRenderer } from "gatsby-plugin-mdx"
+import { graphql, useStaticQuery } from "gatsby"
 const theme = createTheme();
 const sections = [
     { title: 'Education', url: '#' },
@@ -22,12 +17,41 @@ const sections = [
   ];
 export default function content() {
 
+    // 函数式组件的用法
+    const data = useStaticQuery(graphql`
+    query MyQuery {
+        allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+          nodes {
+            rawBody
+            frontmatter {
+              title
+              date
+            }
+            body
+          }
+        }
+      }
+      
+    `)
+
+    const blogContent = () => {
+        const view = data.allMdx.nodes.map((node:any) => (
+            <article key={node.id}>
+                <h2>{node.frontmatter.title}</h2>
+                <p>Posted: {node.frontmatter.date}</p>
+                <MDXRenderer>
+                    {node.body}
+                </MDXRenderer>
+            </article>
+        ))
+        return view
+    }
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Container maxWidth="lg">
         <Header title="Blog" sections={sections} />
-        <p>hahahahah</p>
+        {blogContent()}
       </Container>
       <Footer
         description="All rights reserved"
@@ -35,3 +59,14 @@ export default function content() {
     </ThemeProvider>
   );
 }
+
+// 如果是类组件可以这样用
+// export const query = graphql`
+//   query {
+//     site {
+//       siteMetadata {
+//         description
+//       }
+//     }
+//   }
+// `
