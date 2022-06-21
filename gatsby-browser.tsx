c4ddd526e2f9d2c23e49d50b45df8a1156828ae1
xiayudi <xiayudi@meituan.com>
2022-06-21 17:26:20 +0800

    优化代码块样式

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
 import { MDXProvider } from '@mdx-js/react';
 import Highlight, { defaultProps } from 'prism-react-renderer';
+import theme from "prism-react-renderer/themes/duotoneLight";
+import styled from "styled-components";
+
 /* eslint-disable */
+const Pre = styled.pre`
+  text-align: left;
+  margin: 1em 0;
+  padding: 0.5em;
+  overflow: scroll;
+  border-radius: 1em;
+`;
+
+const Line = styled.div`
+  display: table-row;
+`;
+
+const LineNo = styled.span`
+  display: table-cell;
+  text-align: right;
+  padding-right: 1em;
+  user-select: none;
+  opacity: 0.5;
+`;
+
+const LineContent = styled.span`
+  display: table-cell;
+`;
 const component = {
   pre: props => {
     const className = props.children.props.className || '';
@@ -15,17 +41,21 @@ const component = {
             ? matches.groups.lang
             : ''
         }
+        theme={theme}
       >
         {({ className, style, tokens, getLineProps, getTokenProps }) => (
-          <pre className={className} style={style}>
-            {tokens.map((line, i) => (
-              <div {...getLineProps({ line, key: i })}>
-                {line.map((token, key) => (
-                  <span {...getTokenProps({ token, key })} />
-                ))}
-              </div>
-            ))}
-          </pre>
+            <Pre className={className} style={style}>
+        {tokens.map((line, i) => (
+          <Line key={i} {...getLineProps({ line, key: i })}>
+            <LineNo>{i + 1}</LineNo>
+            <LineContent>
+              {line.map((token, key) => (
+                <span key={key} {...getTokenProps({ token, key })} />
+              ))}
+            </LineContent>
+          </Line>
+        ))}
+      </Pre>
         )}
       </Highlight>
     );
